7f4d0047860cadaa36cf6437564acadfde7acfec
annieline <80864799+annieline@users.noreply.github.com>
2023-02-25 13:16:00 -0500

    added restart button

--- script.js
+++ script.js
@@ -27,19 +27,19 @@ function playRound(playerSelection, computerSelection) {
         case (playerSelection == "SCISSORS" && computerSelection == "PAPER"):
             playerScore++;
             battleText.textContent =`${playerSelection} beats ${computerSelection}, you win!`;
-            pScoreboard.textContent = `Player Score: ${playerScore}`;
-            checkScore();
+            updateScore();
             break;
         default:
             computerScore++;
             battleText.textContent =`${computerSelection} beats ${playerSelection}, you lose!`;
-            cpuScoreboard.textContent = `CPU Score: ${computerScore}`;
-            checkScore();
+            updateScore();
             break;
     }
 }
 
-function checkScore (){
+function updateScore (){
+    pScoreboard.textContent = `Player Score: ${playerScore}`;
+    cpuScoreboard.textContent = `CPU Score: ${computerScore}`;
     if (playerScore == 5 || computerScore == 5) {
         endGame();
     }
@@ -53,6 +53,23 @@ function endGame(){
         battleText.textContent = "Unlucky try again next time!";
     
     btns.forEach(btn => btn.disabled = true);
+    var restartBtn = document.createElement('button');
+    restartBtn.innerText = 'Restart';
+    document.body.appendChild(restartBtn);
+    restartBtn.addEventListener('click', () => {
+        restartGame();
+        restartBtn.remove();
+    });
+}
+
+function restartGame(){
+    playerScore = 0;
+    computerScore = 0;
+    rounds = 0;
+    battleText.textContent = "";
+    roundsBoard.textContent = `Rounds: ${rounds}`;
+    updateScore();
+    btns.forEach(btn => btn.disabled = false);
 }
 
 function buttonPressed(e) {
